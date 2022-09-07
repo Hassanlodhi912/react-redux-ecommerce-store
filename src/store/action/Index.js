c5b348fd6ce39a206fef682c5041e8085c129c23
hassan <muhammadhassanlodhi1232gmail.com>
2022-09-07 18:49:40 +0500

    initial commit

---
+++ src/store/action/Index.js
@@ -0,0 +1,13 @@
+import { ActionTypes } from "../contants/action-type"
+export const setProducts =(products)=>{
+    return{
+        type:ActionTypes.SET_PRODUCTS,
+        payload : products
+    }
+}
+export const selectedProduct =(products)=>{
+    return{
+        type:ActionTypes.SELECTED_PRODUCT,
+        payload : products,
+    }
+}
